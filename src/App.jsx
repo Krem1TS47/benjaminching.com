a29634cb25acbffdcae382a8428f57ae9810c46a
Benjamin Ching <benbenching@gmail.com>
2025-05-04 16:32:59 -0700

    React and TailwindCSS Setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+    return (
+        <h1 className="text-4xl md:text-6xl font-bold mb-4 relative z-10">
+            <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-indigo-600">
+                Benjamin Ching
+            </span>
+        </h1>
+    );
+}
+
+export default App
